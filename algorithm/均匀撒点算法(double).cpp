60c378610a660138d2e6018188f4b9282441939d
ethan <18201521626@139.com>
2018-12-26 17:33:19 +0800

    更新程序，优化为o(n),n=1e7时可算出小数点后十位
    完美！！zzeh出品，不需要写高精度除高精度

--- algorithm/均匀撒点算法(double).cpp
+++ algorithm/均匀撒点算法(double).cpp
@@ -1,18 +1,13 @@
 #include<cstdio>
-long long n,sq,cnt=0;
-const int NR=100000;
-long long r[NR];
-inline long long fastsq(int x){
-    if(r[x]==0 && x<NR)r[x]=x*x;
-    return (x<NR?r[x]:(x*x));
-}
+#include<cmath>
+long long n,sq,lastsq=0,cnt=0;
 int main(){
-    //memset(r,0,sizeof(r));
-    scanf("%lld",&n);
-    sq=n*n;cnt+=(sq+n)/2;
-    for(int i=1;i<=n;i++)
-        for(int j=n-i+1;j<=n;j++)
-  			  cnt+=(fastsq(i)+fastsq(j)<=sq);
-    printf("%lld %lld %.14f",cnt,sq,double(cnt)*4/sq);
+    scanf("%lld",&n);sq=n*n;
+    for(int i=1;i<=n;i++){
+        lastsq+=i+i-1;
+        int j=sqrt(sq-lastsq);
+        cnt+=j+1;
+    }
+    printf("%lld*4 divide by %lld\nPi=%.14f",cnt,sq,double(cnt)*4/sq);
   	return 0;
 }
