10518d45cffb8a189141b31b49c8007caad922ba
Guoyh06 <46080958+Guoyh06@users.noreply.github.com>
2018-12-27 13:05:10 +0800

    finish mul,add,div

--- template/bigdouble.cpp
+++ template/bigdouble.cpp
@@ -1,46 +1,81 @@
-# include <queue>
 # include <cstdio>
-# include <stack>
 # include <cstring>
 # include <algorithm>
-# define LL long long
-# define F(i, a, b) for (int i = a; i < b; i++)
 using namespace std;
-const int NR = 1000;
+const int NR = 5050;
 struct lld{
-	int s[NR * 2 + 5];
-	int l, r;
-	lld(){
-		l = NR - 1;
-		r = NR;
-		memset(s, 0, sizeof(s));
-	}
-	void operator = (const double &x){
-		int a = (int)x;
-		double c = x;
-		while (a){
-			s[r++] = a % 10;
-			a /= 10;
-		}
-		c = c - (int)c;
-		while (c != 0){
-			c *= 10;
-			int b = (int)c;
-			s[l--] = b;
-			c = c - b;
-		}
-	}
-	void print(int x){
-		for (int i = r - 1; i >= NR - x; i--){
-			if (i == NR - 1) printf(".");
-			printf("%d", s[i]);
-		}
-	}
+    int l, r;
+    int s[NR + 505];
+    lld(){
+        l = r = 0;
+        memset(s, 0, sizeof(s));
+    }
+    void operator = (const int &x) {
+        int p = x;
+        while (p){
+            s[NR + r++] = p % 10;
+            p /= 10;
+        }
+    }
+    lld operator + (const lld &x) const{
+        lld q;
+        bool flag = false;
+        q.l = min(x.l, l);
+        int c = 0;
+        for (int i = q.l; i < 505; i++){
+            if (i >= r && i >= x.r && c == 0) break;
+            int p = s[NR + i] + x.s[NR + i] + c;
+            q.s[NR + i] = p % 10;
+            c = p / 10;
+            if (q.s[NR + i] != 0) flag  = true;
+            if (!flag) q.l++;
+            q.r = i + 1;
+        }
+        return q;
+    }
+    lld operator * (const int &x) const{
+        lld q;
+        int c = 0;
+        bool flag = false;
+        for (int i = l; i < 505; i++){
+            if (i >= r && c == 0) break;
+            int p = s[NR + i] * x + c;
+            q.s[NR + i] = p % 10;
+            c = p / 10;
+            if (q.s[NR + i] != 0) flag = true;
+            if (!flag) q.l++;
+            q.r = i + 1;
+        }
+        return q;
+    }
+    lld operator / (const int &x) const{
+        lld q;
+        q.r = r;
+        bool flag = false;
+        int o = 0;
+        for (int i = r - 1; i >= -NR; i--){
+            if (i < l && o == 0) break;
+            o = o * 10 + s[NR + i];
+            q.s[NR + i] = o / x;
+            o = o % x;
+            if (q.s[NR + i] != 0) flag = true;
+            if (!flag) q.r--;
+            q.l = i;
+        }
+        return q;
+    }
+    void print(int x){
+        if (r == 0) printf("0");
+        for (int i = r - 1; i >= x; i--){
+            if (i == -1) printf(".");
+            printf("%d", s[NR + i]);
+        }
+        puts("");
+    }
+    void print(){
+        print(l);
+    }
 };
 int main(){
-	lld a;
-	a = 3.14159265358979;
-	a.print(14);
-	return 0;
+    return 0;
 }
-
