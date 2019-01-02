d1f478efdbb6c7788172780965d3f4321c22275a
deadpool123 <43267021+deadpool233@users.noreply.github.com>
2019-01-03 07:40:42 +0800

    Update 格雷戈里-莱布尼茨无穷级数.cpp

--- algorithm/格雷戈里-莱布尼茨无穷级数.cpp
+++ algorithm/格雷戈里-莱布尼茨无穷级数.cpp
@@ -1,13 +1,11 @@
-#include <cstdio>
-#include <algorithm>
-#include <iostream>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
-int main(int argc, char const *argv[])
+int main()
 {
 	//格雷戈里 - 莱布尼茨无穷级数
 	//π = (4/1) - (4/3) + (4/5)...
-	int n;scanf("%d",&n);
+	int n;
+	scanf("%d",&n);
 	double pi=0;
 	for (int i=1;i<=n;i++){
 		pi+=(i&1?4.0/(i*2-1):0);
