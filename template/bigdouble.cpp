0745a9cd47dda0d811564e14a46c40c841bed625
ethan-enhe <18201521626@139.com>
2019-01-03 09:34:42 +0800

    fix div bug

--- template/bigdouble.cpp
+++ template/bigdouble.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 const int DC = 10;
 struct lld{
-    static const int NR = 30050;
+    static const int NR = 30050,accuRacy=300;
     static char str[NR+510];
     int l, r;
     int s[NR + 505];
@@ -45,7 +45,7 @@ struct lld{
             if (i == -1) printf(".");
             if (s[NR + i] < 10)
                 printf("%d", s[NR + i]);
-            else printf("%c\n", s[NR + i] + 'A');
+            else printf("**%d**", s[NR+i]);
         }
         puts("");
     }
@@ -56,7 +56,7 @@ struct lld{
             if (i == -1) printf(".");
             if (s[NR + i] < 10)
                 printf("%d", s[NR + i]);
-            else printf("%c\n", s[NR + i] + 'A');
+            else printf("**%d**", s[NR+i]);
         }
         puts("");
     }
@@ -161,31 +161,35 @@ struct lld{
         return q;
     }
     lld operator / (const lld &x) const{
-        lld q, o;
-        bool flag = false;
-        for (int i = r - 1; i >= -NR; i--){
+        lld q,o,save_x;
+        save_x.r=x.r-x.l;
+        for(int i=0;i<save_x.r;i++)
+            save_x.s[NR+i]=x.s[NR+x.l+i];
+        while(save_x.s[save_x.r+NR-1]==0 && save_x.r>=0)save_x.r--;
+        q.r=r-x.l;q.l=-accuRacy;
+        for (int i = r - 1; i >= -accuRacy; i--){
             o = o * DC;
             o = o + s[NR + i];
+            //若长度不足则跳过
             if((o.r-o.l)<(x.r-x.l))continue;
-            while(!(o < x)){
-                q.s[NR + i]++;
-                o = o - x;
+            int cnt=0;
+            while(!(o < save_x)){
+                cnt++;
+                o = o - save_x;
             }
-            if (!flag && q.s[NR + i] != 0){
-                q.r = max(q.r, i + 1);
-                flag=1;
-            }
-            q.l = (i<0?i:0);
+            q.s[NR + i - x.l]+=cnt;
             if (o.l == o.r) break;
         }
-        for(int i=l;i<r;i++){
+        for(int i=q.l;i<q.r;i++){
             q.s[NR+i+1]+=q.s[NR+i]/DC;
             q.s[NR+i]%=DC;
             if(q.s[NR+q.r])q.r++;
         }
-        while(q.s[NR+q.l]==0)q.l++;
+        while(q.r>0 && q.s[NR+q.r-1]==0)q.r--;
+        while(q.l<0 && q.s[NR+q.l]==0)q.l++;
         return q;
     }
+    
 };
 char lld::str[NR+510];
 int main(){
