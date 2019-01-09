e05f53055e8156b151d72667b77cb97fc3249177
Edsger-Wybe-Dijkstra <40728083+Edsger-Wybe-Dijkstra@users.noreply.github.com>
2019-01-09 13:20:26 +0800

    fix bug

--- algorithm/目前最优算法.cpp
+++ algorithm/目前最优算法.cpp
@@ -2,50 +2,154 @@
 # include <cstring>
 # define max(a,b) (a>b?a:b)
 # define min(a,b) (a<b?a:b)
-const int NR = 10050;
+const int DC = 10;
 struct lld{
+    static const int NR = 30050, MAXR= 550,accuRacy=300;
+    static char str[NR+MAXR];
     int l, r;
-    int s[NR + 505];
+    int s[NR + MAXR];
     lld(){
         l = r = 0;
         memset(s, 0, sizeof(s));
     }
-    void operator = (const int &x) {
+    lld(int x){
+        l = r = 0;
+        memset(s, 0, sizeof(s));
         int p = x;
         while (p){
-            s[NR + r++] = p % 10;
-            p /= 10;
+            s[NR + r++] = p % DC;
+            p /= DC;
         }
+    }
+    void read(){
+        scanf("%s",str);
+        int len=strlen(str);
+        int dot=0;bool f=0;
+        for(dot = dot; dot < len; dot++)//find where "." is
+            if(str[dot]=='.'){
+                f=1;
+                break;
+            }
+        l=dot-len+f;r=dot;
+        for(int i=0;i<dot;i++)
+            s[NR+r-i-1]=str[i]-'0';
+        for(int i=dot+1;i<len;i++)
+            s[NR+r-i]=str[i]-'0';
+        while(r>0 && s[NR+r-1]==0)r--;
+        while(l<0 && s[NR+l]==0)l++;
+    }
+    void print(int x){
+        if (r <= 0) printf("0");
+        for (int i = max(r - 1, -1); i >= -x; i--){
+            if (i == -1) printf(".");
+            if (s[NR + i] < DC)
+                printf("%d", s[NR + i]);
+            else printf("**%d**", s[NR+i]);
+        }
+        puts("");
+    }
+    void print(){
+        int x=-l;
+        if (r <= 0) printf("0");
+        for (int i = max(r - 1, -1); i >= -x; i--){
+            if (i == -1) printf(".");
+            if (s[NR + i] < DC)
+                printf("%d", s[NR + i]);
+            else printf("**%d**", s[NR+i]);
+        }
+        puts("");
+    }
+    void operator = (const int &x) {
+        l=r=0;
+        memset(s, 0, sizeof(s));
+        int p = x;
+        while (p){
+            s[NR + r++] = p % DC;
+            p /= DC;
+        }
+    }
+    bool operator < (const lld &x) const{
+        if (r != x.r) return r < x.r;
+        for (int i = r - 1; i >= min(l, x.l); i--)
+            if (s[NR + i] != x.s[NR + i]) return s[NR + i] < x.s[NR + i];
+        return false;
+    }
+    lld operator + (const int &x) const{
+        int c = x;
+        lld q;
+        q = *this;
+        for (int i = 0; i < NR; i++){
+            if (c == 0) break;
+            int p = s[NR + i] + c;
+            c = p / DC;
+            q.s[NR + i] = p % DC;
+            q.r = max(q.r, i + 1);
+        }
+        return q;
     }
     lld operator + (const lld &x) const{
         lld q;
-        bool flag = false;
-        q.l = min(x.l, l);
-        int c = 0;
-        for (int i = q.l; i < 505; i++){
-            if (i >= r && i >= x.r && c == 0) break;
-            int p = s[NR + i] + x.s[NR + i] + c;
-            q.s[NR + i] = p % 10;
-            c = p / 10;
-            if (q.s[NR + i] != 0) flag  = true;
-            if (!flag) q.l++;
-            q.r = i + 1;
+        q.l = min(x.l, l);q.r=max(x.r,r);
+        for(int i=q.l;i<q.r;i++)
+            q.s[NR+i]=s[NR+i]+x.s[NR+i];
+        bool f=0;
+        for(int i=q.l;i<q.r;i++){
+            if(q.s[NR+i]>=DC){
+                q.s[NR+i]-=DC;
+                q.s[NR+i+1]++;
+            }
+            if(q.s[NR+i] && !f){
+                f=1;
+                q.l=i;
+            }
+            //if(q.s[q.r+NR])
+            //    q.r++;
         }
+        if(q.s[q.r+NR])
+            q.r++;
+        return q;
+    }
+    lld operator - (const lld &x) const{
+        lld q;q.l=min(l,x.l);q.r=max(r,x.r);
+        for(int i=q.l;i<q.r;i++)
+            q.s[i+NR]=s[i+NR]-x.s[i+NR];
+        for(int i=q.l;i<q.r;i++){
+            q.s[NR+i+1]+=q.s[NR+i]/DC;
+            if(q.s[NR+i]<0){
+                q.s[NR+i]+=DC;
+                q.s[NR+i+1]--;
+            }
+        }
+        while(q.r>0 && q.s[NR+q.r-1]==0)q.r--;
+        while(q.l<0 && q.s[NR+q.l]==0)q.l++;
         return q;
     }
     lld operator * (const int &x) const{
         lld q;
         int c = 0;
         bool flag = false;
-        for (int i = l; i < 505; i++){
+        for (int i = l; i < MAXR-10; i++){
             if (i >= r && c == 0) break;
             int p = s[NR + i] * x + c;
-            q.s[NR + i] = p % 10;
-            c = p / 10;
+            q.s[NR + i] = p % DC;
+            c = p / DC;
             if (q.s[NR + i] != 0) flag = true;
             if (!flag) q.l++;
             q.r = i + 1;
         }
+        q.l = min(q.l, 0);
+        return q;
+    }
+    lld operator * (const lld &x) const{
+        lld q;q.l=l+x.l;q.r=r+x.r;
+        for (int i=l;i<r;i++)
+            for(int j=q.l;j<q.r;j++){
+                q.s[NR+i+j]+=s[i+NR]*x.s[j+NR];
+                q.s[NR+i+j+1]+=q.s[NR+i+j]/DC;
+                q.s[NR+i+j]%=DC;
+            }
+        while(q.s[q.l+NR]==0 && q.l<0)q.l++;
+        while(q.s[q.r+NR-1]==0 && q.r>0)q.r--;
         return q;
     }
     lld operator / (const int &x) const{
@@ -53,8 +157,8 @@ struct lld{
         q.r = r;
         bool flag = false;
         int o = 0;
-        for (int i = r - 1; i >= -NR; i--){
-            o = o * 10 + s[NR + i];
+        for (int i = r - 1; i >= -accuRacy; i--){
+            o = o * DC + s[NR + i];
             q.s[NR + i] = o / x;
             o = o % x;
             if (q.s[NR + i] != 0) flag = true;
@@ -63,23 +167,46 @@ struct lld{
         }
         return q;
     }
-    void print(int x){
-        if (r <= 0) printf("0");
-        for (int i = max(r - 1, -1); i >= -x; i--){
-            if (i == -1) printf(".");
-            printf("%d", s[NR + i]);
+    lld operator / (const lld &x) const{
+        lld q,o,save_x;
+        save_x.r=x.r-x.l;
+        for(int i=0;i<save_x.r;i++)
+            save_x.s[NR+i]=x.s[NR+x.l+i];
+        while(save_x.s[save_x.r+NR-1]==0 && save_x.r>0)save_x.r--;
+        q.r=r-x.l;q.l=-accuRacy;
+        for (int i = r - 1; i >= -accuRacy; i--){
+            o = o * DC;
+            o = o + s[NR + i];
+            //若长度不足则跳过
+            if((o.r-o.l)<(save_x.r-save_x.l))continue;
+            int cnt=0;
+            while(!(o < save_x)){
+                cnt++;
+                o = o - save_x;
+            }
+            q.s[NR + i - x.l]+=cnt;
+            if (o.l == o.r) break;
         }
-        puts("");
-    }
-    void print(){
-        print(-l);
+        /*
+        //these 5 lines of code seem to be unnecessary
+        //if this lld '/' went wrong (when printing the answer ,it appears '**')
+        //you can try to put these 5 lines of code into the final version 
+        for(int i=q.l;i<q.r;i++){
+            q.s[NR+i+1]+=q.s[NR+i]/DC;
+            q.s[NR+i]%=DC;
+            if(q.s[NR+q.r])q.r++;
+        }
+        */
+        while(q.r>0 && q.s[NR+q.r-1]==0)q.r--;
+        while(q.l<0 && q.s[NR+q.l]==0)q.l++;
+        return q;
     }
 };
+char lld::str[NR+MAXR];
 lld pi,last;int n;
 int main(){
     pi=2;last=2;
     scanf("%d",&n);
-    //freopen("pi.out","w",stdout);
     for(int i=1;i<=n;i++){
         last=last*i;
         last=last/(2*i+1);
